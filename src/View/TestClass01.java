216d30d101fb125b6382409f56f91b99c0072837
Jatawat Xie <jatawat.safe@mail.kmutt.ac.th>
2019-03-15 14:10:03 +0700

    [ADD] Initial

---
+++ src/View/TestClass01.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author jatawatsafe
+ */
+public class TestClass01 {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test Frame");
+        frame.setSize(400,400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
