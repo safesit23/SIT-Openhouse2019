9e6aa9c8e98e9675d02dae9a30c1fcf32ab79bbc
Booksnatch <35445342+bookappleseed@users.noreply.github.com>
2019-03-19 16:22:09 +0700

    add code

--- src/View/PrimeNumberChecker.java
+++ src/View/PrimeNumberChecker.java
@@ -5,6 +5,8 @@
  */
 package View;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Books
@@ -35,11 +37,12 @@ public class PrimeNumberChecker extends javax.swing.JFrame {
         jButton2 = new javax.swing.JButton();
         jProgressBar1 = new javax.swing.JProgressBar();
         jPanel4 = new javax.swing.JPanel();
-        jTextField2 = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        area = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("IT OpenHouse 2019");
-        setBackground(new java.awt.Color(51, 255, 51));
+        setBackground(new java.awt.Color(51, 0, 204));
 
         jPanel1.setBackground(new java.awt.Color(14, 58, 83));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -81,6 +84,11 @@ public class PrimeNumberChecker extends javax.swing.JFrame {
         jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 300, 90, -1));
 
         jButton2.setText("Clear");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 350, 90, -1));
         jPanel2.add(jProgressBar1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 240, 197, -1));
 
@@ -88,25 +96,16 @@ public class PrimeNumberChecker extends javax.swing.JFrame {
         jPanel4.setForeground(new java.awt.Color(153, 0, 255));
         jPanel4.setAlignmentX(0.0F);
         jPanel4.setAlignmentY(0.0F);
+        jPanel4.setLayout(new java.awt.BorderLayout());
 
-        jTextField2.setText("                  Result");
+        area.setBackground(new java.awt.Color(102, 0, 102));
+        area.setColumns(20);
+        area.setForeground(new java.awt.Color(102, 0, 102));
+        area.setRows(5);
+        area.setBorder(null);
+        jScrollPane1.setViewportView(area);
 
-        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
-        jPanel4.setLayout(jPanel4Layout);
-        jPanel4Layout.setHorizontalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
-                .addContainerGap(136, Short.MAX_VALUE)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(94, 94, 94))
-        );
-        jPanel4Layout.setVerticalGroup(
-            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel4Layout.createSequentialGroup()
-                .addGap(185, 185, 185)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(424, Short.MAX_VALUE))
-        );
+        jPanel4.add(jScrollPane1, java.awt.BorderLayout.CENTER);
 
         jPanel2.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(411, 4, 430, 650));
 
@@ -122,10 +121,32 @@ public class PrimeNumberChecker extends javax.swing.JFrame {
     }//GEN-LAST:event_jTextField1ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
-        jTextField1.getText();
-        jTextField2.setText(jTextField1.getText());
+        // TODO add your handling code here;
+//        jTextField1.getText();
+//        jTextField2.setText(jTextField1.getText());
+
+            String str1 = jTextField1.getText();
+            int num = Integer.parseInt(str1);
+            int flag = 0 ;
+            for (int i = 2; i < num; i++) {
+                if (num % i ==0) {
+                    flag = 1;
+                    area.setText(area.getText() + "\n" + num + "Divided by "+ i);
+                    
+                }
+        }
+            if (flag ==0 ) {
+            area.setText("\n"+num+" is a prime Number ...");
+        }else{
+                area.setText(area.getText() + "\n\nso," + num + " is not a prime number");
+            }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        jTextField1.setText("");
+        area.setText("");
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -163,6 +184,7 @@ public class PrimeNumberChecker extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JTextArea area;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JPanel jPanel1;
@@ -170,7 +192,7 @@ public class PrimeNumberChecker extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JProgressBar jProgressBar1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 }
